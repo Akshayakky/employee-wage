2fa88506de3c362955a7af782eefda45a10d564e
akshayakky <akshaysmpt01@gmail.com>
2020-02-08 18:05:07 +0530

    ADDED : Monthly Wage Calculation Feature

--- employeeWage.sh
+++ employeeWage.sh
@@ -7,19 +7,25 @@ IS_PART_TIME=1;
 IS_FULL_TIME=2;
 
 #VARIABLES
+totalSalary=0;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+numWorkingDays=20;
 
-case $randomCheck in
-		$IS_FULL_TIME)
-			empHrs=8
-			;;
-		$IS_PART_TIME)
-			empHrs=4
-			;;
-		*)
-			empHrs=0
-			;;
-esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	randomCheck=$((RANDOM%3));
+	case $randomCheck in
+			$IS_FULL_TIME)
+				empHrs=8
+				;;
+			$IS_PART_TIME)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+	esac
 
-salary=$(($empHrs*$empRatePerHr));
+	salary=$(($empHrs*$empRatePerHr));
+	totalSalary=$(($totalSalary+$salary));
+done
