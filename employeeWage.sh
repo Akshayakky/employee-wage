637dc36ad6038a298f4dfaadad0d37f904b10d5d
akshayakky <akshaysmpt01@gmail.com>
2020-02-08 17:12:38 +0530

    ADDED : Employee Attendance

--- employeeWage.sh
+++ employeeWage.sh
@@ -2,3 +2,15 @@
 
 echo Welcome to Employee Wage Computation Program
 
+#CONSTANTS
+IS_PRESENT=1
+
+#VARIABLES
+randomCheck=$((RANDOM%2));
+
+if [ $IS_PRESENT -eq $randomCheck ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
