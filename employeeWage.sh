d8f96d5a68a4529f34849e8da853d3dcf3a82fb3
akshayakky <akshaysmpt01@gmail.com>
2020-02-10 13:22:12 +0530

    ADDED : Added Array to Store Daily Wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -13,26 +13,40 @@ NUM_WORKING_DAYS=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 
+#FUNCTION TO RETURN WORK HOURS
 function getWorkingHours() {
 	case $1 in
-			$IS_FULL_TIME)
-				empHrs=8
-				;;
-			$IS_PART_TIME)
-				empHrs=4
-				;;
-			*)
-				empHrs=0
-				;;
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
 	esac
 	echo $empHrs;
 }
 
+#FUNCTION TO CALCULATE DAILY WAGE
+function calculateDailyWage() {
+	local workHrs=$1
+	wage=$(($workHrs*$EMP_RATE_PER_HOUR))
+	echo $wage
+}
+
+#CALCULATING DAILY EMPLOYEE WAGE AND TOTAL EMPLOYEE HOURS TILL MAXIMUM WORKING HOURS OR TOTAL WORKING DAYS IN A MONTH IS REACHED
 while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++));
 	empHrs="$( getWorkingHours $((RANDOM%3)) )";
 	totalEmpHr=$(($totalEmpHr+$empHrs));
+	empDailyWage[$totalWorkingDays]="$( calculateDailyWage $empHrs )"
 done
 
+#CALCULATE TOTAL SALARY FOR A MONTH
 totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HOUR));
+
+#DISPLAY DAILY WAGES
+echo "Daily Wage : " ${empDailyWage[@]}
