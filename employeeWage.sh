db28d28c3c9d6fe2d939e2876360784857791317
akshayakky <akshaysmpt01@gmail.com>
2020-02-10 12:04:16 +0530

    REFACTOR : Added Function To Return Work Hours

--- employeeWage.sh
+++ employeeWage.sh
@@ -13,22 +13,26 @@ NUM_WORKING_DAYS=20;
 totalEmpHr=0;
 totalWorkingDays=0;
 
+function getWorkingHours() {
+	case $1 in
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs;
+}
+
 while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	((totalWorkingDays++))
-	randomCheck=$((RANDOM%3));
-	case $randomCheck in
-			$IS_FULL_TIME)
-				empHrs=8
-				;;
-			$IS_PART_TIME)
-				empHrs=4
-				;;
-			*)
-				empHrs=0
-				;;
-	esac
+	((totalWorkingDays++));
+	empHrs="$( getWorkingHours $((RANDOM%3)) )";
 	totalEmpHr=$(($totalEmpHr+$empHrs));
 done
 
-totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HOUR))
+totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HOUR));
